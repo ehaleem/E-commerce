3e4029aaafab3aeda2df6600e3bf5f0c562b90df
ehaleem <aelhaleem1@gmail.com>
2025-08-30 14:06:37 +0300

    updata

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
+
+import { useFormik } from 'formik'
+import { useContext } from 'react'
+import { CartContext } from '../../Context/CartContext'
 
 export default function Checkout() {
+
+    let { checkout, cartID } = useContext(CartContext)
+
+    let formik = useFormik({
+        initialValues: {
+            details: "",
+            phone: "",
+            city: ""
+        },
+        onSubmit: () => { handelCheckout(cartID, "http://localhost:5173") }
+    })
+    async function handelCheckout(cartID, url) {
+        let { data } = await checkout(cartID, url, formik.values)
+        console.log(data.session.url);
+        location.href = data.session.url
+    }
+
+
+
     return <>
         <div className="w-full ">
             <h1 className=" font-extrabold text-2xl text-gray-700 my-4">Checkout Now</h1>
-            <form className="max-w-md mx-auto mt-10">
+            <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto mt-10">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
-                    <label htmlFor="email" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                    <input type="text" value={formik.values.details} onBlur={formik.handleBlur} onChange={formik.handleChange} name="details" id="details" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
+                    <label htmlFor="details" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Details</label>
                     <span className="font-medium"></span>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
-                    <label htmlFor="password" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                    <input type="tel" value={formik.values.phone} onBlur={formik.handleBlur} onChange={formik.handleChange} name="phone" id="phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
+                    <label htmlFor="phone" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your Phone</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
-                    <label htmlFor="password" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                    <input type="text" value={formik.values.city} onBlur={formik.handleBlur} onChange={formik.handleChange} name="city" id="city" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-emerald-500 appearance-none  focus:outline-none focus:ring-0 focus:border-emerald-500 peer" placeholder=" " required />
+                    <label htmlFor="city" className="left-4 peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Your City</label>
                 </div>
-                    <button type="submit" className="mb-3  text-white bg-emerald-500 hover:emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full md:w-1/2 sm:w-auto px-5 py-2.5 text-center ">Checkout
-                    </button>
-                
-        </form >
-    </div >
+                <button type="submit" className="mb-3  text-white bg-emerald-500 hover:emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full md:w-1/2 sm:w-auto px-5 py-2.5 text-center ">Checkout
+                </button>
+
+            </form >
+        </div >
 
     </>
 }
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -13,6 +13,7 @@ axios.interceptors.request.use((config) => {
 export let CartContext = createContext()
 export default function CartContextProvider(props) {
     const [cartNumber, setCartNumber] = useState(0)
+    const [cartID, setCartID] = useState(0)
 
 
 
@@ -25,6 +26,9 @@ export default function CartContextProvider(props) {
     async function getProduct() {
         let respons = await axios.get("https://ecommerce.routemisr.com/api/v1/cart")
         setCartNumber(respons.data.numOfCartItems)
+        console.log(respons.data);
+        setCartID(respons.data.cartId)
+        
         return respons
 
     }
@@ -39,13 +43,19 @@ export default function CartContextProvider(props) {
     async function deletAllProduct() {
         return await axios.delete(`https://ecommerce.routemisr.com/api/v1/cart`)
     }
+    
+    async function checkout(cartId,url,formData){
+        return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${url}`,{
+            shippingAddress: formData
+        })
+    }
 
     useEffect(() => {
         getProduct()
     }, [])
 
 
-    return <CartContext.Provider value={{ addToCart, getProduct, updataCountProduct, deletProduct, deletAllProduct, cartNumber, setCartNumber }}>
+    return <CartContext.Provider value={{ addToCart, getProduct, updataCountProduct, deletProduct, deletAllProduct, cartNumber, setCartNumber, cartID ,checkout}}>
         {props.children}
     </CartContext.Provider>
 }
